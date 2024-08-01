00ba3c7b6250b222aad0866ce5978fcaa671af0e
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-08-01 13:43:40 +0200

    fix claims

--- src/Modules/Hexalith.EasyAuthentication.Shared/UserInfo.cs
+++ src/Modules/Hexalith.EasyAuthentication.Shared/UserInfo.cs
@@ -1,5 +1,6 @@
 ﻿namespace Hexalith.EasyAuthentication.Shared;
 
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 
 /// <summary>
@@ -12,6 +13,11 @@ public sealed class UserInfo
     /// Gets the claim type for the user's name.
     /// </summary>
     public static string NameClaimType => "name";
+
+    /// <summary>
+    /// Gets the claim type for the user's email.
+    /// </summary>
+    public static string EmailClaimType => ClaimTypes.Email;
 
     /// <summary>
     /// Gets the claim type for the user's ID.
@@ -33,12 +39,15 @@ public sealed class UserInfo
     /// </summary>
     /// <param name="principal">The <see cref="ClaimsPrincipal"/> representing the authenticated user.</param>
     /// <returns>A new <see cref="UserInfo"/> instance.</returns>
-    public static UserInfo FromClaimsPrincipal(ClaimsPrincipal principal) =>
-        new()
+    public static UserInfo FromClaimsPrincipal([NotNull] ClaimsPrincipal principal)
+    {
+        ArgumentNullException.ThrowIfNull(principal);
+        return new()
         {
-            UserId = GetRequiredClaim(principal, UserIdClaimType),
-            Name = principal.Identity.Name,
+            UserId = GetRequiredUserId(principal),
+            Name = GetRequiredName(principal),
         };
+    }
 
     /// <summary>
     /// Converts the <see cref="UserInfo"/> instance to a <see cref="ClaimsPrincipal"/>.
@@ -47,13 +56,65 @@ public sealed class UserInfo
     public ClaimsPrincipal ToClaimsPrincipal() =>
         new(new ClaimsIdentity(
             [
-                new Claim(UserIdClaimType, UserId),
-                new Claim(NameClaimType, Name),
+                new Claim(UserIdClaimType, UserId ?? string.Empty),
+                new Claim(NameClaimType, Name ?? string.Empty),
             ],
             authenticationType: nameof(UserInfo),
             nameType: NameClaimType,
             roleType: null));
 
-    private static string GetRequiredClaim(ClaimsPrincipal principal, string claimType) =>
-        principal.FindFirst(claimType)?.Value ?? throw new InvalidOperationException($"Could not find required '{claimType}' claim.");
+    private static string GetRequiredName(ClaimsPrincipal principal)
+    {
+        if (!string.IsNullOrWhiteSpace(principal.Identity?.Name))
+        {
+            return principal.Identity.Name;
+        }
+
+        string? name = principal.FindFirst(NameClaimType)?.Value;
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            return name;
+        }
+
+        name = principal.FindFirst(ClaimTypes.Name)?.Value;
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            return name;
+        }
+
+        name = principal.FindFirst(ClaimTypes.Surname)?.Value;
+        string? givenName = principal.FindFirst(ClaimTypes.GivenName)?.Value;
+        if (!string.IsNullOrWhiteSpace(givenName))
+        {
+            return string.IsNullOrWhiteSpace(name) ? givenName : $"{givenName} {name}";
+        }
+
+        name = principal.FindAll(ClaimTypes.Email).Where(p => p.Value.Contains('@')).FirstOrDefault()?.Value;
+        return !string.IsNullOrWhiteSpace(name) ? name : throw new InvalidOperationException($"Could not find required name claim.");
+    }
+
+    private static string GetRequiredUserId(ClaimsPrincipal principal)
+    {
+        string? name = principal.FindFirst(NameClaimType)?.Value;
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            return name;
+        }
+
+        name = principal.FindFirst(ClaimTypes.Name)?.Value;
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            return name;
+        }
+
+        name = principal.FindFirst(ClaimTypes.Surname)?.Value;
+        string? givenName = principal.FindFirst(ClaimTypes.GivenName)?.Value;
+        if (!string.IsNullOrWhiteSpace(givenName))
+        {
+            return string.IsNullOrWhiteSpace(name) ? givenName : $"{givenName} {name}";
+        }
+
+        name = principal.FindAll(ClaimTypes.Email).Where(p => p.Value.Contains('@')).FirstOrDefault()?.Value;
+        return !string.IsNullOrWhiteSpace(name) ? name : throw new InvalidOperationException($"Could not find required name claim.");
+    }
 }
